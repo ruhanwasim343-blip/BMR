2dd216438296e3e70cd2fcf33f2ee26d453fadc2
Ruhan Wasim <ruhanwasim343@gmail.com>
2026-04-10 15:34:49 +0500

    Latest

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,83 +1,54 @@
 import {
-    DropdownMenu,
-    DropdownMenuTrigger,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu"
+
 import { Link } from "react-router-dom"
 
-import { Button } from "@/components/ui/button"
-
 export default function Dropdown() {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <img src="/public/Menu.svg" alt="" />
-            </DropdownMenuTrigger>
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <img src="/Menu.svg" alt="menu" className="cursor-pointer" />
+      </DropdownMenuTrigger>
 
-            <DropdownMenuContent className="w-60 mt-6" align="end">
+      <DropdownMenuContent className="w-60 mt-6" align="end">
+        <DropdownMenuSeparator />
 
+        <DropdownMenuItem>
+          <Link to="/">Home</Link>
+        </DropdownMenuItem>
 
-                <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link to="/about-us">About</Link>
+        </DropdownMenuItem>
 
-                <DropdownMenuItem>
-                    <li>
-                        <Link to="/" className="text-black text-sm hover:text-white transition">
-                            Home
-                        </Link>
-                    </li>
-                </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to="/frameworks">Framework</Link>
+        </DropdownMenuItem>
 
-                <DropdownMenuItem>
-                    <li>
-                        <Link to="/about-us" className="text-black text-sm hover:text-white transition">
-                            About
-                        </Link>
-                    </li>
-                </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to="/services">Services</Link>
+        </DropdownMenuItem>
 
-                <DropdownMenuItem>
-                    <li>
-                        <Link to="/frameworks" className="text-black text-sm hover:text-white transition">
-                            Framework
-                        </Link>
-                    </li>
-                </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to="/insights">Insights</Link>
+        </DropdownMenuItem>
 
-                <DropdownMenuItem>
-                    <li>
-                        <Link to="/services" className="text-black text-sm hover:text-white transition">
-                            Services
-                        </Link>
-                    </li>
-                </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to="/contact">Contact</Link>
+        </DropdownMenuItem>
 
-                <DropdownMenuItem>
-                    <li>
-                        <Link to="/insights" className="text-black text-sm hover:text-white transition">
-                            Insights
-                        </Link>
-                    </li>
-                </DropdownMenuItem>
+        <DropdownMenuSeparator />
 
-                <DropdownMenuItem>
-                    <li>
-                        <Link to="/contact" className="text-black text-sm hover:text-white transition">
-                            Contact
-                        </Link>
-                    </li>
-                </DropdownMenuItem>
-
-
-                <DropdownMenuSeparator />
-
-                <DropdownMenuItem className="text-red-500 focus:text-red-500">
-                    <img src="/public/assets/log-out.svg" alt="" />
-                    Logout
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    )
+        <DropdownMenuItem className="text-red-500">
+          <img src="/assets/log-out.svg" alt="" className="mr-2 w-4 h-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
 }
